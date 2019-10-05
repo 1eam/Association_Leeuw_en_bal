5a15f669321740d45be063edf78f688860ac1d21
Esther <esther191299@gmail.com>
2019-10-05 11:58:44 +0200

    first Commit - Basic functionality

---
+++ src/association/Lion.java
@@ -0,0 +1,13 @@
+package association;
+
+public class Lion {
+	private String name;
+	
+	public Lion (String n) {
+		this.name = n;
+	}
+	
+	public void printName() {
+		System.out.println("This lions name is " + this.name);
+	}
+}
---
+++ src/association/Main.java
@@ -0,0 +1,11 @@
+package association;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Lion lionEsther = new Lion("Esther");
+		
+		lionEsther.printName();
+	}
+
+}
